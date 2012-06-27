c99b196332a578cc621b655e852eececadb77fc6
Sergey Zwezdin <sergey@zwezdin.com>
2012-06-27 21:32:09 +0600

    First release

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ Knockout-MultiModels
 ====================
 
 Plugin for Knockout JS library which allows to use few view models for single view.
+
+Usage
+------
+
+Just use **ko.attach("<name>", viewModel)** and **ko.detach("<name>")** methods instead of **ko.applyBindings()**.
+
+To assign HTML element with view model use **data-model** attribute with name of view model.
+
+Dependencies
+------------
+
+* Jquery 1.7.1+
+* Livequery 1.1.1+
+* Knockout JS 2.1.0+
+
+Live demo
+---------
+
+Please, use **examples/knockout.multimodels.htm** to see live demo.
+
+
+Links:
+------
+
+* [Knockout JS](http://knockoutjs.com/)
+* [Knockout JS (github)](https://github.com/SteveSanderson/knockout/)
+
+License: MIT [http://www.opensource.org/licenses/mit-license.php](http://www.opensource.org/licenses/mit-license.php)
---
+++ src/knockout.multimodels-0.1.js
@@ -0,0 +1,77 @@
+/*
+ * MultiModels plugin for Knockout JS
+ * 
+ * Author: Sergey Zwezdin (2012)
+ *
+ * https://github.com/sergun/Knockout-MultiModels
+ *
+ * License: MIT http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+(function () {
+    function checkDependencies() {
+        /// <summary>Checks dependencies for the plugin.</summary>
+
+        if (typeof ($) !== "function")
+            throw "Knockout JS MultiModels plugin: jQuery not found. Please ensure jQuery is referenced before the knockout.multimodels.js file.";
+
+        if (typeof ($.fn.livequery) != "function")
+            throw "Knockout JS MultiModels plugin: livequery not found. Please ensure livequery is referenced before the knockout.multimodels.js file.";
+
+        if ((typeof (ko.observable) != "function") || (typeof (ko.applyBindings) != "function"))
+            throw "Knockout JS MultiModels plugin: Knockout JS not found. Please ensure knockout js is referenced before the knockout.multimodels.js file.";
+    }
+
+    var _viewModels = {};
+
+    function checkViewModelName(name) {
+        /// <summary>Checks name of view model.</summary>
+
+        var nameExpression = /^[a-z]+[a-z0-9_]*$/i;
+
+        if (!nameExpression.test(name))
+            throw "Knockout JS MultiModels plugin: view model name is incorrect."
+    }
+
+
+    function refreshBindings(viewModelName) {
+        /// <summary>Updates bindings for specific view model name.</summary>
+
+        $("*[data-model]").each(function () {
+            var currentViewModelName = $(this).attr("data-model");
+
+            if (currentViewModelName == viewModelName) {
+                var currentViewModel = _viewModels[currentViewModelName];
+                var currentElement = $(this).get(0);
+
+                ko.cleanNode(currentElement);
+
+                if (currentViewModel != null)
+                    ko.applyBindings(currentViewModel, currentElement);
+            }
+        });
+    };
+
+    checkDependencies();
+
+    ko.attach = function (viewModelName, viewModel) {
+        /// <summary>Attaches view model.</summary>
+
+        checkViewModelName(viewModelName);
+        _viewModels[viewModelName] = viewModel;
+        refreshBindings(viewModelName);
+    };
+
+    ko.detach = function (viewModelName) {
+        /// <summary>Detaches view model.</summary>
+
+        checkViewModelName(viewModelName);
+        _viewModels[viewModelName] = null;
+        refreshBindings(viewModelName);
+    };
+
+    $("*[data-model]").livequery(function () {
+        refreshBindings($(this).attr("data-model"));
+    });
+})();
